91f37d4fe73aa5fb02188b88fd58ff98b711a629
Sarah H <smharmon@ucsc.edu>
2016-07-14 11:55:21 -0400

    update initial phaser file import

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var AspPhaserGenerator = require('./index');
-var aspGame = fs.readFileSync('./test/fixtures/asp-game-4.lp', 'utf8');
-var initialPhaserFile = fs.readFileSync('./test/fixtures/initial-phaser-file-3.json', 'utf8');
+var aspGame = fs.readFileSync('./test/fixtures/generated-3.lp', 'utf8');
+var initialPhaserFile = fs.readFileSync('./test/fixtures/initial-phaser-file-generated.json', 'utf8');
 
 var generator = new AspPhaserGenerator(aspGame, initialPhaserFile);
 var phaserProgram = generator.generate(true);
