bf4528eca7dfcf341116f70649aa1fb838b7302d
Sarah H <smharmon@ucsc.edu>
2016-07-20 15:44:57 -0400

    update initial phaser to single file

--- example.js
+++ example.js
@@ -8,7 +8,7 @@ Travel: 5
 Lecture: 4
 */
 var aspGame = fs.readFileSync('./test/fixtures/generated-complete-1.lp', 'utf8');
-var initialPhaserFile = fs.readFileSync('./test/fixtures/initial-phaser-file-generated.json', 'utf8');
+var initialPhaserFile = fs.readFileSync('./test/fixtures/initial-phaser-file.json', 'utf8');
 
 var generator = new AspPhaserGenerator(aspGame, initialPhaserFile);
 var phaserProgram = generator.generate(true);
